f16cf31deac51499d602843bd9ff2b68715ea701
unknown <alejandra.slimcd@gmail.com>
2026-08-05 22:08:48 -0500

    feat(ui): create first Sentinel panel

--- resources/sentinel_core/client.lua
+++ resources/sentinel_core/client.lua
@@ -1,12 +1,14 @@
+local menuVisible = false
+
 CreateThread(function()
 
     Wait(3000)
 
     TriggerEvent('chat:addMessage', {
-        color = {0, 170, 255},
+        color = {0,170,255},
         args = {
             "Sentinel AI",
-            "Bienvenida. Sentinel AI está listo."
+            "Bienvenida. Pulsa F1 para abrir Sentinel."
         }
     })
 
@@ -14,15 +16,31 @@ CreateThread(function()
 
         Wait(0)
 
-        if IsControlJustPressed(0, 288) then -- F1
+        if IsControlJustPressed(0,288) then
+            menuVisible = not menuVisible
+        end
 
-            TriggerEvent('chat:addMessage', {
-                color = {255, 255, 0},
-                args = {
-                    "Sentinel AI",
-                    "Has pulsado F1."
-                }
-            })
+        if menuVisible then
+
+            DrawRect(
+                0.5,
+                0.35,
+                0.30,
+                0.25,
+                0,
+                0,
+                0,
+                180
+            )
+
+            SetTextFont(4)
+            SetTextScale(0.55,0.55)
+            SetTextColour(255,255,255,255)
+            SetTextCentre(true)
+
+            BeginTextCommandDisplayText("STRING")
+            AddTextComponentSubstringPlayerName("SENTINEL AI")
+            EndTextCommandDisplayText(0.5,0.25)
 
         end
 
