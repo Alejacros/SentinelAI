bc908786006378f10aae53e2ef1258d9f47c8909
unknown <alejandra.slimcd@gmail.com>
2026-08-05 22:04:52 -0500

    feat(core): detect F1 key

--- resources/sentinel_core/client.lua
+++ resources/sentinel_core/client.lua
@@ -4,11 +4,28 @@ CreateThread(function()
 
     TriggerEvent('chat:addMessage', {
         color = {0, 170, 255},
-        multiline = true,
         args = {
             "Sentinel AI",
             "Bienvenida. Sentinel AI está listo."
         }
     })
+
+    while true do
+
+        Wait(0)
+
+        if IsControlJustPressed(0, 288) then -- F1
+
+            TriggerEvent('chat:addMessage', {
+                color = {255, 255, 0},
+                args = {
+                    "Sentinel AI",
+                    "Has pulsado F1."
+                }
+            })
+
+        end
+
+    end
 
 end)
